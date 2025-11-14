d08e378333413cd6adaeb1dc3f2bcc6da62dde17
Róisín Grannell <r.grannell2@gmail.com>
2025-11-14 15:00:41 +0000

    removes old build artifacts

--- bs/build_fork.sh
+++ bs/build_fork.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 
+rm dist_fork/css/*
+rm dist_fork/js/*
+
 publication_id="$(jq -r .publication_id ./manifest/env.json)"
 deno run -A /home/rg/Code/tribbledb/cli.ts stringify < "./manifest/triples.${publication_id}.json" > "./manifest/tribbles.${publication_id}.txt"
 
