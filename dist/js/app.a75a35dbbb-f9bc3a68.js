029089a459d31406c28b1a6c087fc84542802f18
Róisín Grannell <r.grannell2@gmail.com>
2026-05-24 15:42:54 +0100

    adds label

--- dist/js/app.a75a35dbbb-f9bc3a68.js
+++ dist/js/app.a75a35dbbb-f9bc3a68.js
@@ -13,4 +13,4 @@ leaflet/dist/leaflet-src.js:
    * (c) 2010-2023 Vladimir Agafonkin, (c) 2010-2011 CloudMade
    *)
 */
-//# sourceMappingURL=app.9c3fcaec29-f9bc3a68.js.map
+//# sourceMappingURL=app.a75a35dbbb-f9bc3a68.js.map
--- dist/js/sw.a75a35dbbb-f9bc3a68.js
+++ dist/js/sw.a75a35dbbb-f9bc3a68.js
@@ -7,8 +7,8 @@ const CACHEABLE_RESOURCES = [
   "/icons/favicon-32x32.png",
   "/favicon.ico",
   "/favicon-32x32.png",
-  "/dist/css/style.9c3fcaec29-f9bc3a68.css",
-  "/dist/js/app.9c3fcaec29-f9bc3a68.js",
+  "/dist/css/style.a75a35dbbb-f9bc3a68.css",
+  "/dist/js/app.a75a35dbbb-f9bc3a68.js",
   "https://photos-cdn.rgrannell.xyz/a4a694cea4.webp",
 ];
 
