4fe891265f26aee4d2bdb57a2d00b38c5a51efe9
Róisín Grannell <r.grannell2@gmail.com>
2026-08-05 12:39:54 +0000

    updates Dublin Zoo, Las Palmas Museums, Puerto de la Cruz and 3 more

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sw-cache-2e59eaca0f-6d45af75";
+const CACHE_NAME = "sw-cache-f6c0d81f24-6d45af75";
 const CACHEABLE_RESOURCES = [
   "/icons/android-chrome-192x192.png",
   "/icons/android-chrome-512x512.png",
@@ -6,7 +6,7 @@ const CACHEABLE_RESOURCES = [
   "/icons/favicon-16x16.png",
   "/icons/favicon-32x32.png",
   "/favicon.ico",
-  "/dist/js/app.2e59eaca0f-6d45af75.js",
+  "/dist/js/app.f6c0d81f24-6d45af75.js",
   // the two page-hero banners, from ts/constants/banners.ts
   "https://photos-cdn.rgrannell.xyz/d6cf0f7cc7.webp",
   "https://photos-cdn.rgrannell.xyz/6744c802d1.webp",
