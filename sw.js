a281f3278a014f309a4f16ab5876862ad39c2bcb
Róisín Grannell <r.grannell2@gmail.com>
2026-08-05 13:07:35 +0000

    updates Bray Air Show, Bristol, Lincoln Park Zoo, Chicago and 2 more

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sw-cache-f6c0d81f24-6d45af75";
+const CACHE_NAME = "sw-cache-c773f8b01d-87cbb4c5";
 const CACHEABLE_RESOURCES = [
   "/icons/android-chrome-192x192.png",
   "/icons/android-chrome-512x512.png",
@@ -6,7 +6,7 @@ const CACHEABLE_RESOURCES = [
   "/icons/favicon-16x16.png",
   "/icons/favicon-32x32.png",
   "/favicon.ico",
-  "/dist/js/app.f6c0d81f24-6d45af75.js",
+  "/dist/js/app.c773f8b01d-87cbb4c5.js",
   // the two page-hero banners, from ts/constants/banners.ts
   "https://photos-cdn.rgrannell.xyz/d6cf0f7cc7.webp",
   "https://photos-cdn.rgrannell.xyz/6744c802d1.webp",
