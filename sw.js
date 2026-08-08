cddfd6f646be394d395a2ecda6a4ee8600326daa
Róisín Grannell <r.grannell2@gmail.com>
2026-08-08 09:37:08 +0000

    updates International Crane Foundation, Wisconsin, Shedd Aquarium & Science Museum, Chicago, Terryland Park and 1 more

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sw-cache-07d4dadfa3-a4880a80";
+const CACHE_NAME = "sw-cache-165faf8e12-a4880a80";
 const CACHEABLE_RESOURCES = [
   "/icons/android-chrome-192x192.png",
   "/icons/android-chrome-512x512.png",
@@ -6,7 +6,7 @@ const CACHEABLE_RESOURCES = [
   "/icons/favicon-16x16.png",
   "/icons/favicon-32x32.png",
   "/favicon.ico",
-  "/dist/js/app.07d4dadfa3-a4880a80.js",
+  "/dist/js/app.165faf8e12-a4880a80.js",
   // the two page-hero banners, from ts/constants/banners.ts
   "https://photos-cdn.rgrannell.xyz/d6cf0f7cc7.webp",
   "https://photos-cdn.rgrannell.xyz/6744c802d1.webp",
