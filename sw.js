731f7ae2424c414dd684e7750bea681d4baf14c0
Róisín Grannell <r.grannell2@gmail.com>
2026-08-08 14:29:46 +0000

    publish artifacts 2026-08-08

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sw-cache-442101150b-dd5c331c";
+const CACHE_NAME = "sw-cache-ed390f6007-1560c510";
 const CACHEABLE_RESOURCES = [
   "/icons/android-chrome-192x192.png",
   "/icons/android-chrome-512x512.png",
@@ -6,7 +6,7 @@ const CACHEABLE_RESOURCES = [
   "/icons/favicon-16x16.png",
   "/icons/favicon-32x32.png",
   "/favicon.ico",
-  "/dist/js/app.442101150b-dd5c331c.js",
+  "/dist/js/app.ed390f6007-1560c510.js",
   // the two page-hero banners, from ts/constants/banners.ts
   "https://photos-cdn.rgrannell.xyz/d6cf0f7cc7.webp",
   "https://photos-cdn.rgrannell.xyz/6744c802d1.webp",
